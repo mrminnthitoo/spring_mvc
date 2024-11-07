bf0433a8828156970df2f4aa2bb8fc0c7ad63e51
Minn Thit Oo <mto.forlearning@gmail.com>
2024-11-07 12:22:13 +0630

    thymeleaf view demo

--- src/main/java/com/minnthitoo/spring_mvc/controller/ViewDemoController.java
+++ src/main/java/com/minnthitoo/spring_mvc/controller/ViewDemoController.java
@@ -1,9 +1,12 @@
 package com.minnthitoo.spring_mvc.controller;
 
+import com.minnthitoo.spring_mvc.model.BookDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Calendar;
 
 @Controller
 @RequestMapping("/view-demo")
@@ -12,7 +15,15 @@ public class ViewDemoController {
     @GetMapping
     String viewDemo(Model model){
         model.addAttribute("message", "Hello From Model");
+        model.addAttribute("today", Calendar.getInstance());
         return "viewDemo";
+    }
+
+    @GetMapping("/object")
+    String demoObject(Model model){
+        BookDto bookDto = new BookDto(1L, "Title 1", "Author 1", "Description 1");
+        model.addAttribute("book", bookDto);
+        return "view-demo/books";
     }
 
 }
