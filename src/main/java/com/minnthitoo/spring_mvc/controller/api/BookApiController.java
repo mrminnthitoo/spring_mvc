dcee3fc8f60a6558459e765ce0135baec0c12456
mrminnthitoo <mrminnthitoo@gmail.com>
2024-11-12 20:56:22 +0630

    custom api documentation add

--- src/main/java/com/minnthitoo/spring_mvc/controller/api/BookApiController.java
+++ src/main/java/com/minnthitoo/spring_mvc/controller/api/BookApiController.java
@@ -3,6 +3,11 @@ package com.minnthitoo.spring_mvc.controller.api;
 import com.minnthitoo.spring_mvc.controller.api.exception.BookNotFoundException;
 import com.minnthitoo.spring_mvc.model.dto.BookDto;
 import com.minnthitoo.spring_mvc.service.BookService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -24,6 +29,17 @@ public class BookApiController {
         return this.bookService.getAllBooks();
     }
 
+
+    @Operation(summary = "Get a book by its id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found the book",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = BookDto.class))}),
+            @ApiResponse(responseCode = "400", description = "Invalid id supplied.",
+                    content = @Content),
+            @ApiResponse(responseCode = "404", description = "Book not found.",
+                    content = @Content),
+    })
     @GetMapping("/{bookId}")
     ResponseEntity<BookDto> getBookById(@PathVariable Long bookId) throws BookNotFoundException{
         Optional<BookDto> result = this.bookService.getBookById(bookId);
