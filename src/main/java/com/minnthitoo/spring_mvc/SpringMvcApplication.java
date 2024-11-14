a8aabe5ed8c6b87f81d0d0446e9edb5792dd9b1e
mrminnthitoo <mrminnthitoo@gmail.com>
2024-11-14 22:03:01 +0630

    schedule task

--- src/main/java/com/minnthitoo/spring_mvc/SpringMvcApplication.java
+++ src/main/java/com/minnthitoo/spring_mvc/SpringMvcApplication.java
@@ -2,7 +2,9 @@ package com.minnthitoo.spring_mvc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class SpringMvcApplication {
 
